32bbd8edc148eb5ffebcb8617c21658cedb15b88
Peter Tran <tran.peter08@gmail.com>
2020-11-16 01:01:29 -0800

    created documentation router

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,11 +2,13 @@ const express = require('express');
 const userRouter = require('./userRoutes');
 const petRouter = require('./petRoutes');
 const thirdPartyRouter = require('./thirdPartyRoutes');
+const apiDocsRoutes = require('./apiDocsRoutes');
 
 const router = express.Router();
 
 router.use('/users', userRouter);
 router.use('/pets', petRouter);
 router.use('/3rd-party-api', thirdPartyRouter);
+router.use('/docs', apiDocsRoutes);
 
 module.exports = router;
