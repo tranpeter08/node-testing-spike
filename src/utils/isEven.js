36bcc1a1339dacc0e2feee8c77ff2d257b488c94
Peter Tran <tran.peter08@gmail.com>
2020-11-16 01:01:51 -0800

    added documentation

--- src/utils/isEven.js
+++ src/utils/isEven.js
@@ -1,4 +1,10 @@
-function isEven(num = 0) {
+/**
+ * Determines if an integer is even.
+ * @param {number} num - An integer
+ * @returns {boolean}
+ */
+
+function isEven(num) {
   if (typeof num !== 'number' || !Number.isInteger(num)) {
     throw new Error('Must be an integer');
   }
