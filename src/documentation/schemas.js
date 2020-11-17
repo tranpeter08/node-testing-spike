d9c64dcc94292faa35df965df9db03db106b50f5
Peter Tran <tran.peter08@gmail.com>
2020-11-16 23:48:38 -0800

    created pet request body schema

--- src/documentation/schemas.js
+++ src/documentation/schemas.js
@@ -1,10 +1,37 @@
 module.exports = {
+  PetsReqBodyCreate: {
+    type: 'object',
+    description: 'Request body for creating a pet',
+    properties: {
+      name: {
+        description: 'The pet\'s name',
+        type: 'string',
+        example: 'Hachiko'
+      },
+      type: {
+        description: 'The pet type',
+        type: 'string',
+        example: 'Loyal dog'
+      },
+    }
+  },
   ErrorResp: {
     type: 'object',
     description: 'Error response',
     properties: {
       message: {
-        type: 'string'
+        type: 'string',
+        example: 'Some error message'
+      },
+      errCode: {
+        type: 'string',
+        example: 'SOME_ERROR_CODE'
+      },
+      statusCode: {
+        type: 'integer',
+        min: 100,
+        max: 599,
+        example: 400
       }
     }
   }
