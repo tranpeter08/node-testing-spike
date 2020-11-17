e3074296dd4b6279b9783f28a61a499db61698fb
Peter Tran <tran.peter08@gmail.com>
2020-11-16 23:47:58 -0800

    created parameters.js for swagger components

---
+++ src/documentation/parameters.js
@@ -0,0 +1,12 @@
+module.exports = {
+  userIdQueryParam: {
+    name: 'userId',
+    in: 'query',
+    description: 'A user\'s ID',
+    required: true,
+    schema: {
+      type: 'string',
+      example: '5fb37b8672c339da1f6a487d'
+    }
+  }
+}
